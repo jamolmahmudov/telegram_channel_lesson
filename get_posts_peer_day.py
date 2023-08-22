d931447e730c598fd74c8762e99c336b785c5c55
jamolmahmudov <jamolmahmudov27102003@gmail.com>
2023-08-22 12:23:46 +0500

    posts_peer_day

--- get_posts_peer_day.py
+++ get_posts_peer_day.py
@@ -1,7 +1,8 @@
 from read_data import fromJson
+import datetime
 
 
-def get_posts_peer_day(day:str)->int:
+def get_posts_peer_day(path,day:str)->int:
     """
     Return the number of posts for given day
 
@@ -11,4 +12,12 @@ def get_posts_peer_day(day:str)->int:
     Returns: 
         dict: a dictionary with the number of posts for each month
     """
-    return
+    post = 0
+    data=fromJson(path)['messages']   
+    for i in data:
+        x=i["date"].split('T')[0]
+        if  x== day:
+            post += 1
+
+    return post
+print(get_posts_peer_day('data/result.json','2022-09-30'))
